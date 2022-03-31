49f34eb63e28cd779a61628b34ec31acb5d77d68
Sanzhar <semid46@gmail.com>
2022-03-31 21:06:19 +0600

    init project

---
+++ src/main/java/kz/sdu/activitymonitoringsdu/ActivityMonitoringSduApplication.java
@@ -0,0 +1,13 @@
+package kz.sdu.activitymonitoringsdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityMonitoringSduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityMonitoringSduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/sdu/activitymonitoringsdu/ActivityMonitoringSduApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.sdu.activitymonitoringsdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityMonitoringSduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
